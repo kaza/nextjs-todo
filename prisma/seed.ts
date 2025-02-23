d7be62f86bd203ed5eb454c3d25e5a68847b8e65
Almir Kazazic <kazazic@gmail.com>
2025-02-23 20:32:59 +0100

    init

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const passwordHash = await bcrypt.hash("password123", 10);
+
+  const user = await prisma.user.upsert({
+    where: { email: "test@example.com" },
+    update: {},
+    create: {
+      email: "test@example.com",
+      name: "Test User",
+      password: passwordHash,
+    },
+  });
+
+  await prisma.todo.createMany({
+    data: [
+      { title: "First Task", detail: "This is my first task", userId: user.id },
+      { title: "Second Task", detail: "This is my second task", userId: user.id },
+    ],
+  });
+
+  console.log("Database has been seeded. 🌱");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
